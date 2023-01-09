2acbad177de63163f41a6ad5025efa4ff5bbbfdf
phutx <phu.tx@orai.io>
2023-01-09 17:18:04 +0700

    add logic

--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,36 @@
-use cosmwasm_schema::{cw_serde, QueryResponses};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use crate::state::{Entry, Priority, Status};
 
-#[cw_serde]
-pub struct InstantiateMsg {}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub owner: Option<String>,
+}
 
-#[cw_serde]
-pub enum ExecuteMsg {}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    NewEntry {description: String, priority: Option<Priority>},
+    UpdateEntry { id: u64, description: Option<String>, status: Option<Status>, priority: Option<Priority> },
+    DeleteEntry { id: u64 }
+}
 
-#[cw_serde]
-#[derive(QueryResponses)]
-pub enum QueryMsg {}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    QueryEntry {id: u64},
+    QueryList {start_after: Option<u64>, limit: Option<u32>},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EntryResponse {
+    pub id: u64,
+    pub description: String,
+    pub status: Status,
+    pub priority: Priority,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ListResponse {
+    pub entries: Vec<Entry>,
+}
